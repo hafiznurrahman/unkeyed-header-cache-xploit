5172b43545f5d12437423ba8460f75bef9880804
Hafiz Nur Rahman <hfznrrhmn12@gmail.com>
2025-07-07 21:02:14 +0700

    return random letters and integers

--- utils/cache_buster.py
+++ utils/cache_buster.py
@@ -1,6 +1,9 @@
 import random
+import string
 
-def cache_buster():
+def cache_buster_value():
+    characters = string.ascii_letters
     random_integer = random.randint(100000, 999999)
-    parameter = f"uhcxispoisoning={random_integer}"
-    return parameter
+    random_letters = ''.join(random.choices(characters, k=6))
+    
+    return f"{random_letters}{random_integer}"
